7bf705a21cb1e053d0f42b8677d2253220aa8f66
atrox3d <atrox3d@gmail.com>
2021-03-20 13:44:22 +0100

    route tutorial

--- hello.py
+++ hello.py
@@ -1,8 +1,44 @@
 from flask import Flask
+import config
 
 app = Flask(__name__)
+print(__name__)
 
-
+# http://192.168.1.10:5000/
 @app.route('/')
 def hello_world():
     return "hello, world!"
+
+
+# http://192.168.1.10:5000/bye
+@app.route('/bye')
+def bye():
+    return "bye!"
+
+
+# http://192.168.1.10:5000/{name}
+@app.route('/<name>')
+def username(name):
+    return f"hello {name}"
+
+
+# http://192.168.1.10:5000/username/{name}
+@app.route('/username/<name>')
+def greet(name):
+    return f"hello {name}"
+
+
+# http://192.168.1.10:5000/path/{some/words/separated/by/slash}
+@app.route('/path/<path:path>')
+def get_path(path):
+    return path
+
+
+# http://192.168.1.10:5000/variables/{name}/{any number}
+@app.route('/variables/<name>/<int:number>')
+def variables(name, number):
+    return f"{name}, {number}"
+
+
+if __name__ == "__main__":
+    app.run(host=config.HOST_IP, debug=True)
